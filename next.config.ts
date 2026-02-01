5993305b0e60ad76ca758d1b50eb47a8563ecf6b
bartr <bartr@microsoft.com>
2026-01-31 20:46:58 -0600

    updated sample

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  output: "standalone", // Required for Docker deployment
-};
-
-export default nextConfig;
